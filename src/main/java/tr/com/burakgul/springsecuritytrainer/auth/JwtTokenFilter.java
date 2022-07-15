053b93f775a5d5a70b26effba8dded988a950dfa
Burak GÜL <burakkggul@gmail.com>
2022-07-15 16:37:32 +0300

    unnecessary import fix

--- src/main/java/tr/com/burakgul/springsecuritytrainer/auth/JwtTokenFilter.java
+++ src/main/java/tr/com/burakgul/springsecuritytrainer/auth/JwtTokenFilter.java
@@ -7,7 +7,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-import tr.com.burakgul.springsecuritytrainer.models.User;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
